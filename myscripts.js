78f550bb68a822997549bff89c8ba66be24aa1a6
Horatio Caine <horatio.caine@hotmail.co.uk>
2023-08-31 15:54:49 +0900

    CHANGE color, add --var to css, game win logic js

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,6 @@
 const tiles = document.querySelectorAll("div.tile");
 
-// module that displays the gameboard 
+// module that displays the gameboard and allows playrs to draw markers
 const gameboard = (() => { 
     const markers = ["x", "o", "x", "o", "x", "o", "x", "o", "x"];
 
@@ -26,9 +26,41 @@ const gameboard = (() => {
 const gameLogic = (() => {
 
 const checkWin = () => {
-    
         if (tiles[0].innerHTML === tiles[1].innerHTML && tiles[0].innerHTML === tiles[2].innerHTML && tiles[0].innerHTML != "") {
             alert("The winner is " + tiles[0].innerHTML);
+            return;
+        }
+        if (tiles[3].innerHTML === tiles[4].innerHTML && tiles[3].innerHTML === tiles[5].innerHTML && tiles[3].innerHTML != "") {
+            alert("The winner is " + tiles[3].innerHTML);
+            return;
+        }
+        if (tiles[6].innerHTML === tiles[7].innerHTML && tiles[6].innerHTML === tiles[8].innerHTML && tiles[6].innerHTML != "") {
+            alert("The winner is " + tiles[6].innerHTML);
+            return;
+        }
+        if (tiles[0].innerHTML === tiles[3].innerHTML && tiles[0].innerHTML === tiles[6].innerHTML && tiles[0].innerHTML != "") {
+            alert("The winner is " + tiles[0].innerHTML);
+            return;
+        }
+        if (tiles[1].innerHTML === tiles[4].innerHTML && tiles[1].innerHTML === tiles[7].innerHTML && tiles[1].innerHTML != "") {
+            alert("The winner is " + tiles[1].innerHTML);
+            return;
+        }
+        if (tiles[2].innerHTML === tiles[5].innerHTML && tiles[2].innerHTML === tiles[8].innerHTML && tiles[2].innerHTML != "") {
+            alert("The winner is " + tiles[2].innerHTML);
+            return;
+        }
+        if (tiles[0].innerHTML === tiles[4].innerHTML && tiles[0].innerHTML === tiles[8].innerHTML && tiles[0].innerHTML != "") {
+            alert("The winner is " + tiles[0].innerHTML);
+            return;
+        }
+        if (tiles[2].innerHTML === tiles[4].innerHTML && tiles[2].innerHTML === tiles[6].innerHTML && tiles[2].innerHTML != "") {
+            alert("The winner is " + tiles[2].innerHTML);
+            return;
+        }
+        if (gameboard.markers.length === 0) {
+            alert("Draw.")
+            return;
         }
     }
     return {
@@ -37,7 +69,10 @@ const checkWin = () => {
 })();
 
 // factory that returns player objects
-const playerFactory = (marker) => {
-    return {marker};
-
+const playerFactory = (name, marker) => {
+    return { name, marker };
 }
+
+const horatio = playerFactory('horatio', 'x');
+console.log(horatio.name);
+console.log(horatio.marker);
